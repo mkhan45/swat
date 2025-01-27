b03fc65b582a14ba78675bcffabd6b174c0f19bd
Mikail Khan <mikail@mikail-khan.com>
2025-01-27 18:27:23 -0500

    lots of typechecker

--- lib/typecheck.ml
+++ lib/typecheck.ml
@@ -4,6 +4,13 @@ open Core
 exception UnunificationException of string
 exception Undefined
 
+type missing_write = { expected: (varname * tp); got: (varname * tp) }
+exception MissingWrite of missing_write
+
+exception MatchError
+
+type type_mismatch = { var: varname; wrote: tp; read: tp }
+
 module Substs = struct
   include Map.Make(String)
 end
@@ -70,23 +77,69 @@ let%test _ = type_subtype type_pos type_nat
 let%test _ = not (type_subtype type_nat type_pos)
 
 module Hamburger = struct
-  include Map.Make(String)
+  type t = (string, tp, String.comparator_witness) Map.t
+  type error = TypeMismatch of type_mismatch | NotFound of varname | Unused of t
+  exception TypeMismatch of error
 
-  type tt = tp t
+  let _smush (_left : t) (_right : t) : t option = raise Undefined 
 
-  let smush (left : tt) (right : tt) : tt option = raise Undefined 
-    
+  let rec check_complete (env : t) (reads : (varname * tp) list) : error option = match reads with
+  | [] when Map.is_empty env -> None
+  | [] (* otherwise *) -> Some (Unused env)
+  | (read_var, read_tp) :: rest -> begin match Map.find env read_var with
+                                   | Some write_tp when type_subtype read_tp write_tp -> check_complete (Map.remove env read_var) rest
+                                   | Some write_tp -> Some (TypeMismatch { var = read_var; wrote = write_tp; read = read_tp })
+                                   | None -> Some (NotFound read_var)
+                                   end
 end
 
-let typecheck (env : Hamburger.tt) (com : cmd) (var, vartp) : Hamburger.tt option =
-  match com with
-  | Id (x, y) -> (match Map.find env x with
-                  | Some xtp -> (match Map.find env y with
-                                 | Some ytp -> if type_subtype ytp xtp
-                                                 then Some (Map.remove env y)
-                                                 else None
-                                 | None -> None)
-                  | None -> None)
-  |
+module Procs = struct
+    type t = (string, tp list, String.comparator_witness) Map.t
 
+    let _args_subtype (_proc_name : string) (_args : varname list) ~(_env: Hamburger.t) = raise Undefined
+end
 
+(*let typecheck (env : Hamburger.t) (com : cmd) (var, vartp) : Hamburger.t option = None*)
+
+type inferred_tp = { write : (varname * tp); read : (varname * tp) list }
+
+let typecheck (_procs : Procs.t) (_dest : tp) (_body : cmd) : bool =
+    let rec infer (com : cmd) (env : Hamburger.t) : inferred_tp = 
+        let ( !! ) v = match Map.find env v with
+                       | Some r -> r
+                       | None -> raise Hamburger.(TypeMismatch (NotFound v))
+        in
+        match com with
+        | Id (into, from) -> { write = (into, !!from); read = [(from, !!from)] }
+        | Read (from, cases) -> 
+                let new_env = Map.remove env from in
+                begin match (!!from, cases) with
+                | (One, [(UnitPat, com)]) -> infer com new_env
+                | (Times (lt, rt), [(PairPat (lv, rv), com)]) -> infer com (new_env |> Map.set ~key:lv ~data:lt |> Map.set ~key:rv ~data:rt)
+                | (Plus tp_cases, match_cases) when phys_equal (List.length tp_cases) (List.length match_cases) -> 
+                        (* TODO: instead of zipping, map over match cases, all should be injpat, then check against type env *)
+                        (*let cases = List.zip_exn tp_cases match_cases |> List.map ~f:(fun ((_l, t), (p, com)) -> infer com (bind new_env t p)) in*)
+                        raise Undefined
+                | _ -> raise MatchError
+                end
+        | Write (into, tipe_pat) ->
+                begin match tipe_pat with
+                | UnitPat -> { write = (into, One); read = [] }
+                | PairPat (left, right) -> { write = (into, Times (!!left, !!right)); read = [(left, !!left); (right, !!right)] }
+                | InjPat (l, v) -> let t = Plus [(l, !!v)] in { write = (into, t); read = [(v, !!v)] }
+                end
+        | Cut (new_v, new_t, left, right) ->
+                let l_infer = infer left env in
+                let (l_write_var, l_write_tp) = l_infer.write in
+                if not ((String.equal new_v l_write_var) && (type_subtype l_write_tp new_t)) 
+                then 
+                    raise (MissingWrite { expected = (new_v, new_t); got = l_infer.write })
+                else begin match Hamburger.check_complete env l_infer.read with
+                  | Some err -> raise (Hamburger.TypeMismatch err)
+                  | None -> infer right (Map.set env ~key:new_v ~data:new_t)
+                end
+        | Call (_proc, dest, args) -> 
+                (* TODO: actually check args, dest type against proc *)
+                { write = (dest, !!dest); read = args |> List.map ~f:(fun arg -> (arg, !!arg)) }
+    in 
+    raise Undefined
--- lib/typecheck.mli
+++ lib/typecheck.mli
@@ -4,14 +4,3 @@ open Ast
 val type_equals: tp -> tp -> bool
 val type_subtype: tp -> tp -> bool
 val type_unify: tp -> tp -> (string * tp) list
-
-module Hamburger : sig
-  include Map.S
-
-  type tt = tp t
-  (* functions *)
-  val smush : tt -> tt -> tt option
-end
-
-(* typechecker *)
-val typecheck : Hamburger.tt -> cmd -> (varname * tp) -> Hamburger.tt option
