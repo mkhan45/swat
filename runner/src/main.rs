d35e6f2f32859f08766e93cb9d706d32b5d8a416
Mikail Khan <mikail@mikail-khan.com>
2025-04-17 01:48:23 -0400

    almost kinda works

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -18,17 +18,20 @@ fn main() {
             unsafe {
                 let sz = mem.data_size(&store) / 4;
                 for i in 0..sz {
-                    *((mem_ptr as *mut i32).add(i)) = 8;
+                    *((mem_ptr as *mut i32).add(i)) = 2;
                 }
             }
             *store.data_mut() = (mem_ptr, 0);
 
+            // TODO: figure out when returning wasm addrs, i32 offsets
             let print_i32 = Func::wrap(&mut store, |x: i32| println!("{x}"));
-            let alloc = Func::wrap(&mut store, |mut caller: Caller<'_, (*mut u8, i32)>| unsafe {
+            let alloc = Func::wrap(&mut store, |mut caller: Caller<'_, (*mut u8, i32)>, v1: i32, v2: i32| unsafe {
                 let &mut (ref mut base, ref mut cur) = caller.data_mut();
                 let base = (*base) as *mut i32;
                 let cur_ptr = base.add(*cur as usize);
                 let next_free = *cur_ptr;
+                *cur_ptr = v1;
+                *(cur_ptr.add(1)) = v2;
                 let res = *cur;
                 *cur += next_free;
 
@@ -39,7 +42,7 @@ fn main() {
                     let next_offs = *base.add(fl_ptr as usize);
                     fl_ptr += next_offs;
                 }
-                println!("Alloc'd {res} {:?}", fl);
+                println!("Alloc'd {res} with ({v1},{v2}) {:?}", fl);
                 res
             });
             let free = Func::wrap(&mut store, |mut caller: Caller<'_, (*mut u8, i32)>, offs: i32| unsafe {
@@ -67,6 +70,15 @@ fn main() {
             let main = instance.get_typed_func::<(i32, i32), i32>(&mut store, "main").unwrap();
             let res = main.call(&mut store, (5, 0)).unwrap();
             println!("{res}");
+
+            unsafe {
+                let mut mem_buf: Vec<i32> = vec![];
+                let mut ptr = mem_ptr as *mut i32;
+                for i in 0..30 {
+                    mem_buf.push(*(ptr.add(i)));
+                }
+                println!("mem: {:?}", mem_buf);
+            }
         }
         _ => {
             println!("expected wasm file");
