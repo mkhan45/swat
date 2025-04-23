3f5cedac7e8fae074f5a8262d6f0e1b213c672de
Mikail Khan <mikail@mikail-khan.com>
2025-04-22 23:29:59 -0400

    ack

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 echo "wrote $1.wat"
 wasm-opt --enable-bulk-memory --enable-tail-call -O2 "$1.wat" -S -o "$1_opt.wat"
 echo "wrote $1_opt.wat"
-./runner/target/debug/runner $1_opt.wat
+time ./runner/target/release/runner $1_opt.wat
 # ./runner/target/debug/runner $1.wat
