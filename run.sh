0d707cd12947803d188ccbee5dc84f86e4efdbdf
Mikail Khan <mikail@mikail-khan.com>
2025-06-23 00:37:46 -0400

    wrote todos

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ rm "$1_opt.wat"
 echo "wrote $1.wat"
 # --disable-custom-descriptors doesn't seem to work for removing (exact) annotation,
 # use wasm-opt commit 2bef62a882ef30d2ed70d7813642913774ee78f5 for now
-wasm-opt --enable-bulk-memory --enable-tail-call --enable-reference-types --enable-gc --disable-custom-descriptors -O2 "$1.wat" -S -o "$1_opt.wat"
+wasm-opt --enable-bulk-memory --enable-tail-call --enable-reference-types --enable-gc -O2 "$1.wat" -S -o "$1_opt.wat"
 echo "wrote $1_opt.wat"
 time ./runner/target/release/runner $1_opt.wat
 # ./runner/target/debug/runner $1.wat
